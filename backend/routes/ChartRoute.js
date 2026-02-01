b617d2767e9b5773f6dadf744dd970238888ca78
Akash1347 <alampallyakash@gmail.com>
2026-02-01 22:00:13 +0530

    fixed bug

--- backend/routes/ChartRoute.js
+++ backend/routes/ChartRoute.js
@@ -1,5 +1,5 @@
 const route = require('express').Router();
-const { candleChart } = require('../controllers/chartController');
+const { candleChart } = require('../controllers/ChartController');
 route.get('/:symbol' ,candleChart);
 
 module.exports = route;
